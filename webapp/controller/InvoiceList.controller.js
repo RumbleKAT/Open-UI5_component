89a7cf1e4f2f3cbf3e709f8508b3c506700671cc
RumbleKAT <reki318@naver.com>
2020-01-06 22:53:16 +0900

    add grouping and search filter

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,19 +1,34 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
-], function (Controller, JSONModel, formatter) {
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter , FilterOperator) {
     "use strict";
 
     return Controller.extend("com.myorg.myUI5App.controller.InvoiceList", {
-        formatter : formatter,
+        formatter : formatter, // 기존 정의한 formatter 모듈을 그대로 사용함
+
         onInit : function () {
             var oViewModel = new JSONModel({
                 currency: "EUR"
             });
 
             this.getView().setModel(oViewModel, "view");
+        },
+        onFilterInvoices : function(oEvent){
+            var aFilter = [];
+            var query = oEvent.getParameter("query"); //매개변수 query로 전달함 
+            if(query){
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, query)); //대소문자를 구분하지 않는다.
+            }
+
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
+
 
     });
 });
